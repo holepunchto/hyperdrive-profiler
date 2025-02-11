bb2b648527f7802e52d1575cce1922a5e010ffc2
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-02-11 11:58:36 +0100

    Clean up

--- bin.js
+++ bin.js
@@ -19,7 +19,11 @@ const cmd = command('hyperdrive-profiler',
   flag('--interval|-i [statsIntervalSec]', 'Interval (in seconds) at which to print the performance stats (default 10)'),
   async function ({ args, flags }) {
     const key = IdEnc.decode(args.key)
-    const statsIntervalMs = 1000 * (parseInt(flags.statsIntervalSec || 2))
+    const statsIntervalMs = 1000 * (parseInt(flags.statsIntervalSec || 10))
+
+    const tStart = performance.now()
+    let secTillMetadata = null
+    let secTillFullyDownload = null
 
     const tmpdir = path.join(os.tmpdir(), `hyperdrive-profiler-tmp-${Math.random().toString(16).slice(2)}`)
     console.info(`Profiling hyperdrive download for ${IdEnc.normalize(key)}`)
@@ -38,25 +42,26 @@ const cmd = command('hyperdrive-profiler',
     const drive = new Hyperdrive(store, key)
     const swarm = new Hyperswarm()
     swarm.on('connection', conn => {
-      console.log('Opened connection')
       store.replicate(conn)
       conn.on('error', (e) => {
         console.log(`Connection error: ${e.stack}`)
-      })
-      conn.on('close', () => {
-        console.log('Closed connection')
       })
     })
 
     const swarmStats = new HyperswarmStats(swarm)
 
-    const tStart = performance.now()
     const printStats = () => {
       const elapsedSec = (performance.now() - tStart) / 1000
+
+      let timestampsInfo = `General\n  - Runtime: ${elapsedSec.toFixed(2)} seconds`
+      timestampsInfo += `\n  - Metadata found in: ${secTillMetadata.toFixed(2).toString() + ' seconds' || 'unknown (still connecting...)'}`
+      if (secTillFullyDownload) timestampsInfo += `\n  - Fully downloaded in ${secTillFullyDownload.toFixed(2)} seconds`
+
       const udxInfo = getUdxInfo(swarmStats, elapsedSec)
       const swarmInfo = getSwarmInfo(swarmStats)
       const hypercoreInfo = getHypercoreInfo(hypercoreStats)
-      console.log(`Stats after ${elapsedSec.toFixed(2)} seconds:\n ${udxInfo}${swarmInfo}${hypercoreInfo}`)
+      console.log(`${timestampsInfo}\n${udxInfo}${swarmInfo}${hypercoreInfo}`)
+      console.log(`${'-'.repeat(30)}\n`)
     }
     const statsInterval = setInterval(printStats, statsIntervalMs)
 
@@ -66,7 +71,7 @@ const cmd = command('hyperdrive-profiler',
       if (cancelling) console.info('Cancelling before the download is complete...')
       clearInterval(statsInterval)
 
-      console.log('Destroying swarm...')
+      console.log('\nDestroying swarm...')
       await swarm.destroy()
       console.log('Closing corestore...')
       await store.close()
@@ -78,11 +83,14 @@ const cmd = command('hyperdrive-profiler',
 
     swarm.join(drive.discoveryKey, { server: false, client: true })
     if (drive.db.core.length <= 1) await once(drive.db.core, 'append') // DEVNOTE: in theory we could get a not-latest length, but 'good enough'
+    secTillMetadata = (performance.now() - tStart) / 1000
 
     console.info(`Downloading drive version ${drive.version}`)
     await drive.download('/', { wait: true })
-    console.info('Drive fully downloaded')
+
+    secTillFullyDownload = (performance.now() - tStart) / 1000
     cancelling = false
+    goodbye.exit()
   }
 )
 
@@ -118,14 +126,14 @@ function getUdxInfo (swarmStats, elapsedSec) {
   const bytesTx = swarmStats.dhtStats.udxBytesTransmitted
   const bytesTxPerSec = (bytesTx / elapsedSec).toFixed(2)
   const packetsRx = swarmStats.dhtStats.udxPacketsReceived
-  const packetsRxPerSec = (packetsRx / elapsedSec).toFixed(2)
+  const packetsRxPerSec = (packetsRx / elapsedSec).toFixed(0)
   const packetsTx = swarmStats.dhtStats.udxPacketsTransmitted
-  const packetsTxPerSec = (packetsTx / elapsedSec).toFixed(2)
+  const packetsTxPerSec = (packetsTx / elapsedSec).toFixed(0)
 
   const packetsDropped = swarmStats.dhtStats.udxPacketsDropped
   const packetsDroppedPerSec = (packetsDropped / elapsedSec).toFixed(2)
 
-  return `UDX
+  return `Network (UDX)
   - Bytes received: ${byteSize(bytesRx)} (${byteSize(bytesRxPerSec)} / second)
   - Bytes transmitted: ${byteSize(bytesTx)} (${byteSize(bytesTxPerSec)} / second)
   - Packets received: ${packetsRx} (${packetsRxPerSec} / second)
